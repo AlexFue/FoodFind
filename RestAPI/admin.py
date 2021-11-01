ea0864f046b491467c1789e194755154937178e6
Alex <alexespinoza11f@gmail.com>
2021-11-01 10:41:31 -0700

    created models for db and registered them

--- RestAPI/admin.py
+++ RestAPI/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
+from .models import Recipe
+from .models import SavedFoods
+from .models import FoodList
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Recipe)
+admin.site.register(SavedFoods)
+admin.site.register(FoodList)
--- RestAPI/models.py
+++ RestAPI/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    userId = models.AutoField(primary_key=True)
+    name = models.CharField('Name', max_length=100)
+    username = models.CharField('Username', max_length=100)
+    password = models.CharField('Password', max_length=100)
+
+class Recipe(models.Model):
+    recipeId = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    name = models.CharField('Name', max_length=100)
+    description = models.CharField('Description', max_length=500)
+    image = models.ImageField(upload_to='images')
+
+class FoodList(models.Model):
+    foodListId = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    recipes = models.ManyToManyField(Recipe, blank=True)
+
+class SavedFoods(models.Model):
+    savedFoodsId = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    recipes = models.ManyToManyField(Recipe, blank=True)
+
