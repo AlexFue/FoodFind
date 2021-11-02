bac6a1a9a0f3504e9130e2b83254e72daaacec5a
Edward Cluster <ecluster@csumb.edu>
2021-11-02 07:52:46 -0700

    Completed FoodUsersAPI function

--- RestAPI/EdwardAPIs/views.py
+++ RestAPI/EdwardAPIs/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import authenticate, login
 import requests
 
 # files for models, serializer, and views
-# from ..models import / /
+from ..models import Recipe
 from .serializer import RecipeSerializer
 
 from .. import views
@@ -17,12 +17,12 @@ from .. import views
 # Get all of the foods that every user has created
 @api_view(['GET'])
 def foods_users_api(request):
-    # API endpoint: '[url]/apiV1/food-users-api/'
+    # API endpoint: '[url]/apiV1/foods-users-api/'
     if request.method == 'GET':
         print("GET for foods_users_api")
         print("-----------------------")
-        # foods = Recipe.objects.all()
-        # serializer = RecipeSerializer(foods, many=True)
-        # json_obj = json.loads(json.dumps(serializer.data))
-        # print("JSON object:\n",json_obj)
-        # return Response(serializer.data)
+        foods = Recipe.objects.all()
+        serializer = RecipeSerializer(foods, many=True)
+        json_obj = json.loads(json.dumps(serializer.data))
+        print("JSON object:\n",json_obj)
+        return Response(serializer.data)
