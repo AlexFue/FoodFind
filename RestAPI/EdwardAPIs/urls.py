bdd5ecf2a58f1500534f92bcdcf83f20c3138004
Edward Cluster <ecluster@csumb.edu>
2021-11-02 22:46:05 -0700

    Working CreateUserAPI function, Need to add session

--- RestAPI/EdwardAPIs/urls.py
+++ RestAPI/EdwardAPIs/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('view-foods-users-api/', views.foods_users_api, name='foods-users-api'),
+    path('create-users-api/<str:u_name>/<str:u_username>/<str:u_password>/', views.create_user_api, name='create-users-api'),
 ]
