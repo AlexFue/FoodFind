417123b3055c6182683befc8d14183ed41c9fa17
Alex <alexespinoza11f@gmail.com>
2021-11-02 22:07:34 -0700

    function to create a savedfoodlist for a user by a username that is passed in

--- RestAPI/AlexAPIs/views.py
+++ RestAPI/AlexAPIs/views.py
@@ -7,12 +7,24 @@ from .serializers import FoodListSerializer
 from .serializers import SavedFoodsSerializer
 from ..models import User
 from ..models import FoodList
+from ..models import SavedFoods
 
 # Function that creates a foodlist for the username that is passed in
 def createFoodList(username):
     user = User.objects.get(username=username)
     foodList = FoodList(user=user)
     serializer = FoodListSerializer(foodList)
+    if serializer.is_valid():
+        serializer.save()
+        return True
+    return False
+
+
+# Function that creates a saved foodlist for the username that is passed in
+def savedFoodList(username):
+    user = User.objects.get(username=username)
+    savedFoods = SavedFoods(user=user)
+    serializer = SavedFoodsSerializer(savedFoods)
     if serializer.is_valid():
         serializer.save()
         return True
