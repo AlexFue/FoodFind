11aba2b985557438cbe9abc96cfaae22be5c6b11
Onyi A <89602534+Onyi21@users.noreply.github.com>
2021-11-15 18:17:16 -0800

    worked on the create account layout sample

---
+++ android/app/src/main/java/com/example/foodfind/CreateAnAccount.java
@@ -0,0 +1,27 @@
+package com.example.foodfind;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAnAccount extends AppCompatActivity {
+    public Button createaccBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+        createaccBtn = findViewById(R.id.btnCreateAccount);
+
+        createaccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(CreateAnAccount.this, Login.class);
+                startActivity(i);
+            }
+        });
+    }
+}
