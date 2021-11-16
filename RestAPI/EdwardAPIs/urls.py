a685da0e92b9f1c92d1ef27973523abd4e408454
Edward Cluster <ecluster@csumb.edu>
2021-11-16 01:06:35 -0800

    User create account (need to create user's default foodlist) Login created but authentication needs adjustments

--- RestAPI/EdwardAPIs/urls.py
+++ RestAPI/EdwardAPIs/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('view-foods-users-api/', views.foods_users_api, name='foods-users-api'),
     path('create-users-api/<str:u_name>/<str:u_username>/<str:u_password>/', views.create_user_api, name='create-users-api'),
+    path('login-api/<str:u_username>/<str:u_password>/', views.login_api, name='login-api'),
 ]
--- RestAPI/EdwardAPIs/views.py
+++ RestAPI/EdwardAPIs/views.py
@@ -4,11 +4,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import json
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model, update_session_auth_hash
 import requests
 
 # files for models, serializer, and views
 from ..models import Recipe
+from ..models import User
+from ..models import FoodList
 from .serializer import RecipeSerializer
 from .serializer import UserSerializer
 from .. import views
@@ -42,7 +44,37 @@ def create_user_api(request, u_name, u_username, u_password):
         }
         serializer = UserSerializer(data=user_data)
         if serializer.is_valid():
+            # Save the new user
             serializer.save()
             print(json.loads(json.dumps(serializer.data)))
+
+            # Get the user that was just created
+            user_obj = User.objects.get(username=u_username)
+            serializer2 = UserSerializer(user_obj, many=False)
+            new_user_json = json.loads(json.dumps(serializer2.data))
+            print("Getting new user created:\n", new_user_json)
+
+            # create food list
+            # issues creating the food list
+            print("Type: ", type(user_obj))
+            food_list = FoodList.objects.create(new_user_json['userId'])
+
+            # save it
+            food_list.save()
             return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.data, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+
+# Login
+@api_view(['POST'])
+def login_api(request, u_username, u_password):
+    if request.method == 'POST':
+        # API endpoint: '[url]/api/login/'
+        username = u_username
+        password = u_password
+        authenticated = authenticate(username=username, password=password)
+        if authenticated is not None:
+            user = User.objects.get(username=username)
+            serializer = UserSerializer(user, many=False)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response("Not a valid username or password", status=status.HTTP_400_BAD_REQUEST)
