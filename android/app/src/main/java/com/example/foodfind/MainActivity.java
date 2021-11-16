781e1306dcd6b524718b9c3531064b2b85a1ac05
Sagar Prasad <saprasad@csumb.edu>
2021-11-15 18:28:11 -0800

    Created the landing_activity. Started on create food page

--- android/app/src/main/java/com/example/foodfind/MainActivity.java
+++ android/app/src/main/java/com/example/foodfind/MainActivity.java
@@ -3,12 +3,61 @@ package com.example.foodfind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewResult = findViewById(R.id.text_view_result);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://127.0.0.1:8000/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        API api = retrofit.create(API.class);
+        Call<List<Recipe>> call = api.getRecipes();
+
+        call.enqueue(new Callback<List<Recipe>>() {
+            @Override
+            public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
+                if(!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Recipe> recipes = response.body();
+
+                for (Recipe recipe : recipes) {
+                    String content = "";
+                    content += "recipeId: " + recipe.getRecipeId() + "\n";
+                    content += "user: " + recipe.getUser() + "\n";
+                    content += "name: " + recipe.getName() + "\n";
+                    content += "description: " + recipe.getDescription() + "\n";
+                    content += "image: " + recipe.getImage() + "\n\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Recipe>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
     }
 }
